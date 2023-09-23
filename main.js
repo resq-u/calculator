25c1263d7a3e36fe66da76fe08c8459fbae84916
resq-u <jakub12lepianka@wp.pl>
2023-09-23 21:31:35 +0200

    Add exponential values for longer solutions

--- main.js
+++ main.js
@@ -18,23 +18,36 @@ calculate();
 
 function add(a, b){
     let sum = +a + +b;
-    return displayContainer.textContent = sum.toString().slice(0, 12);
+    if (sum.toString().length > 11) {
+        return displayContainer.textContent = sum.toExponential(3);
+    }
+    return displayContainer.textContent = sum.toString().slice(0, 11);
 };
 
 function subtract(a, b){
     let difference = a - b;
-    return displayContainer.textContent = difference.toString().slice(0, 12);
+    if (difference.toString().length > 11) {
+        return displayContainer.textContent = difference.toExponential(3);
+    }
+    return displayContainer.textContent = difference.toString().slice(0, 11);
 }
 
 function multiply(a, b){
     let product = a * b;
-    return displayContainer.textContent = product.toString().slice(0, 12);
+    console.log("lol");
+    if (product.toString().length > 11) {
+        return displayContainer.textContent = product.toExponential(3);
+    }
+    return displayContainer.textContent = product.toString().slice(0, 11);
 }
 
 function divide(a, b){
     let quotient = a / b;
-    
-    return displayContainer.textContent = quotient.toString().slice(0, 12);
+    if (quotient.toString().length > 11) {
+        return displayContainer.textContent = quotient.toExponential(3);
+    }
+    if (b == 0) return displayContainer.textContent = "lmao";
+    return displayContainer.textContent = quotient.toString().slice(0, 11);
 }
 
 function operate(op, first, second){  
@@ -49,16 +62,21 @@ function displayNumbers(){
     numberButtons.forEach(btn => {
        let currentNumber = btn.textContent;
        btn.addEventListener('click', () => {
-            displayContainer.textContent += currentNumber;
-
+            
             if (operator === '') {
+                if (firstNumber.toString().length > 10) return;
                 firstNumber += currentNumber; // 1st phase
-
+                displayContainer.textContent += currentNumber;
             } else if (operator !== '') {
-                if (secondNumber === '') {
-                    displayContainer.textContent = `${secondNumber += currentNumber}`;
-
-                } else if (secondNumber !== '') secondNumber += currentNumber; // 3rd phase
+                    if (secondNumber === '') {  
+                        displayContainer.textContent = `${secondNumber += currentNumber}`;
+                        // clears the previous number from the display to prevent overflow
+                        // by placing first secondNumber
+                    } else if (secondNumber !== '') {
+                        if (secondNumber.toString().length > 10) return;
+                        secondNumber += currentNumber; // 3rd phase
+                        displayContainer.textContent += currentNumber;
+                    }
             }
         });
     });
@@ -83,7 +101,7 @@ function getOperator(){
             if (operator === '') {
                 operator = btn.textContent;
                 displayContainer.textContent += btn.textContent;
-            } else {
+            } else if (operator !== ''){
                 displayContainer.textContent = '\u00A0';
                 firstNumber = operate(operator, firstNumber, secondNumber);  // 4th phase
                 secondNumber = '';
